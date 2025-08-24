9f2effb295fe7e5b0efd159b92d8c9d2ce2de5a3
Alex Giraldo <giraldojhong4@gmail.com>
2025-08-23 22:57:56 -0500

    general

--- LIN.Allo.Shared/Components/Elements/Control.razor.cs
+++ LIN.Allo.Shared/Components/Elements/Control.razor.cs
@@ -1,4 +1,6 @@
-﻿namespace LIN.Allo.Shared.Components.Elements;
+﻿using LIN.Types.Cloud.Identity.Models.Identities;
+
+namespace LIN.Allo.Shared.Components.Elements;
 
 
 public partial class Control : IConversationViewer, IDisposable
