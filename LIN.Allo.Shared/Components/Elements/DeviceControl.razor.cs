244a29aa961432743e5297d8d92b19ba6da87daf
Alex Giraldo <giraldojhong4@gmail.com>
2025-09-20 20:55:11 -0500

    mejoras

---
+++ LIN.Allo.Shared/Components/Elements/DeviceControl.razor.cs
@@ -0,0 +1,71 @@
+﻿namespace LIN.Allo.Shared.Components.Elements;
+
+
+public partial class DeviceControl
+{
+
+    /// <summary>
+    /// Modelo del producto.
+    /// </summary>
+    [Parameter]
+    public DeviceOnAccountModel? Model { get; set; }
+
+
+    /// <summary>
+    /// Evento al hacer click.
+    /// </summary>
+    [Parameter]
+    public Action<DeviceOnAccountModel?>? OnClick { get; set; }
+
+    /// <summary>
+    /// Enviar el evento.
+    /// </summary>
+    private void SendEvent()
+    {
+        OnClick?.Invoke(Model);
+    }
+
+
+
+    /// <summary>
+    /// Obtener el icono.
+    /// </summary>
+    private (string device, string surface) GetImage()
+    {
+        if (Model is null)
+            return ("", "");
+
+        const string path = "_content/LIN.Allo.Shared/devices/{0}.png";
+
+        string device = "";
+        string surface = "";
+
+        switch (Model.OperativeSystem)
+        {
+            case "android":
+                device = "android";
+                break;
+            case "windows":
+                device = "windows";
+                break;
+            case "ios":
+                device = "ios";
+                break;
+            case "linux":
+                device = "linux";
+                break;
+        }
+
+        surface = Model.SurfaceFrom switch
+        {
+            "edge" => "edge",
+            "chrome" => "chrome",
+            "safari" => "safari",
+            "native" => "",
+            _ => "globe",
+        };
+        return (string.Format(path, device), string.IsNullOrWhiteSpace(surface) ? string.Empty : string.Format(path, surface));
+
+    }
+
+}
---
+++ LIN.Allo.Shared/Components/Shared/DevicesDrawer.razor.cs
@@ -0,0 +1,70 @@
+﻿namespace LIN.Allo.Shared.Components.Shared;
+
+public partial class DevicesDrawer
+{
+
+    /// <summary>
+    /// ID del elemento Html.
+    /// </summary>
+    public string _id = $"element-{Guid.NewGuid()}";
+
+
+    /// <summary>
+    /// Lista de dispositivos.
+    /// </summary>
+    [Parameter]
+    public static ReadAllResponse<DeviceOnAccountModel> DevicesList { get; set; } = null!;
+
+
+    /// <summary>
+    /// Evento onclick.
+    /// </summary>
+    [Parameter]
+    public Action<DeviceOnAccountModel> OnInvoke { get; set; } = (d) => { };
+
+
+    /// <summary>
+    /// Es la primer abierta?
+    /// </summary>
+    public bool FirstShow { get; set; } = true;
+
+
+    /// <summary>
+    /// Abrir el elemento.
+    /// </summary>
+    public async void Show()
+    {
+
+        // Abrir el elemento.
+        await JsRuntime.InvokeVoidAsync("showBottomDrawer", _id, DotNetObjectReference.Create(this), $"btn-close-{_id}", "close-all-all");
+
+        // Si es el primer open.
+        if (FirstShow)
+        {
+            _ = GetDevices();
+            FirstShow = false;
+        }
+        StateHasChanged();
+    }
+
+
+    /// <summary>
+    /// Obtener los dispositivos.
+    /// </summary>
+    private async Task<bool> GetDevices()
+    {
+
+        // Items
+        var items = await Access.Communication.Controllers.Profiles.Devices(Access.Communication.Session.Instance.Token);
+
+        // Rellena los items
+        DevicesList = items;
+
+        // Eliminar dispositivo local.
+        items.Models = [.. items.Models.Where(t => t.ConnectionId != Device.ConnectionId)];
+        StateHasChanged();
+        return true;
+
+    }
+
+}
